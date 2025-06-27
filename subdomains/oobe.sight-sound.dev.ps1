6565cfaac5956e8f87d1914859a09ab5eaf0008e
Matthew Miles <matthew.miles@sight-sound.com>
2025-06-27 15:04:13 -0500

    Enhance OOBE configuration UI by adding manufacturer-model display and improving layout for computer details

--- subdomains/oobe.sight-sound.dev.ps1
+++ subdomains/oobe.sight-sound.dev.ps1
@@ -37,7 +37,9 @@ $xaml = @"
                     Background="#FF292939" HorizontalAlignment="Center" Width="330">
                 <StackPanel>
                     <TextBlock Text="Computer Details" FontSize="15" FontWeight="Bold"
-                               Foreground="White" HorizontalAlignment="Center" Margin="0,0,0,10"/>
+                               Foreground="White" HorizontalAlignment="Center" Margin="0,0,0,2"/>
+                    <TextBlock Name="txtManModel" FontSize="13" Foreground="#FFDADADA"
+                               HorizontalAlignment="Center" Margin="0,0,0,10"/>
                     <Grid Margin="0,0,0,2">
                         <Grid.ColumnDefinitions>
                             <ColumnDefinition Width="80"/>
@@ -47,19 +49,13 @@ $xaml = @"
                             <RowDefinition Height="Auto"/>
                             <RowDefinition Height="Auto"/>
                             <RowDefinition Height="Auto"/>
-                            <RowDefinition Height="Auto"/>
-                            <RowDefinition Height="Auto"/>
                         </Grid.RowDefinitions>
-                        <TextBlock Text="Man:"        Grid.Column="0" Grid.Row="0" Foreground="#FFAAAAAA" FontSize="13" Margin="0,0,6,2" HorizontalAlignment="Right" VerticalAlignment="Center"/>
-                        <TextBlock Name="txtManufacturer" Grid.Column="1" Grid.Row="0" Foreground="White" FontSize="13" Margin="0,0,0,2" TextWrapping="Wrap" VerticalAlignment="Center"/>
-                        <TextBlock Text="Model:"      Grid.Column="0" Grid.Row="1" Foreground="#FFAAAAAA" FontSize="13" Margin="0,0,6,2" HorizontalAlignment="Right" VerticalAlignment="Center"/>
-                        <TextBlock Name="txtModel"    Grid.Column="1" Grid.Row="1" Foreground="White" FontSize="13" Margin="0,0,0,2" TextWrapping="Wrap" VerticalAlignment="Center"/>
-                        <TextBlock Text="Serial:"     Grid.Column="0" Grid.Row="2" Foreground="#FFAAAAAA" FontSize="13" Margin="0,0,6,2" HorizontalAlignment="Right" VerticalAlignment="Center"/>
-                        <TextBlock Name="txtSerial"   Grid.Column="1" Grid.Row="2" Foreground="White" FontSize="13" Margin="0,0,0,2" VerticalAlignment="Center"/>
-                        <TextBlock Text="BIOS:"       Grid.Column="0" Grid.Row="3" Foreground="#FFAAAAAA" FontSize="13" Margin="0,0,6,2" HorizontalAlignment="Right" VerticalAlignment="Center"/>
-                        <TextBlock Name="txtBios"     Grid.Column="1" Grid.Row="3" Foreground="White" FontSize="13" Margin="0,0,0,2" TextWrapping="Wrap" VerticalAlignment="Center"/>
-                        <TextBlock Text="CPU:"        Grid.Column="0" Grid.Row="4" Foreground="#FFAAAAAA" FontSize="13" Margin="0,0,6,2" HorizontalAlignment="Right" VerticalAlignment="Center" Name="lblCpuLabel"/>
-                        <TextBlock Name="txtCpu"      Grid.Column="1" Grid.Row="4" Foreground="White" FontSize="13" Margin="0,0,0,2" VerticalAlignment="Center"/>
+                        <TextBlock Text="Serial:"   Grid.Column="0" Grid.Row="0" Foreground="#FFAAAAAA" FontSize="13" Margin="0,0,6,2" HorizontalAlignment="Right" VerticalAlignment="Center"/>
+                        <TextBlock Name="txtSerial" Grid.Column="1" Grid.Row="0" Foreground="White" FontSize="13" Margin="0,0,0,2" VerticalAlignment="Center"/>
+                        <TextBlock Text="BIOS:"     Grid.Column="0" Grid.Row="1" Foreground="#FFAAAAAA" FontSize="13" Margin="0,0,6,2" HorizontalAlignment="Right" VerticalAlignment="Center"/>
+                        <TextBlock Name="txtBios"   Grid.Column="1" Grid.Row="1" Foreground="White" FontSize="13" Margin="0,0,0,2" TextWrapping="Wrap" VerticalAlignment="Center"/>
+                        <TextBlock Text="CPU:"      Grid.Column="0" Grid.Row="2" Foreground="#FFAAAAAA" FontSize="13" Margin="0,0,6,2" HorizontalAlignment="Right" VerticalAlignment="Center" Name="lblCpuLabel"/>
+                        <TextBlock Name="txtCpu"    Grid.Column="1" Grid.Row="2" Foreground="White" FontSize="13" Margin="0,0,0,2" VerticalAlignment="Center"/>
                     </Grid>
                 </StackPanel>
             </Border>
@@ -147,6 +143,7 @@ $txtBios         = Find-Name 'txtBios'
 $lblCpuLabel     = Find-Name 'lblCpuLabel'
 $txtCpu          = Find-Name 'txtCpu'
 $txtWinget       = Find-Name 'txtWinget'
+$txtManModel     = Find-Name 'txtManModel'
 
 # Get Computer Details
 try {
@@ -154,112 +151,131 @@ try {
     $bios    = Get-WmiObject -Class Win32_BIOS
     $proc    = Get-WmiObject -Class Win32_Processor | Select-Object -First 1
 
-    $txtManufacturer.Text  = $compSys.Manufacturer
-    $txtModel.Text         = $compSys.Model
-    $txtSerial.Text        = $bios.SerialNumber
-    $txtBios.Text          = $bios.SMBIOSBIOSVersion
+    if ($txtManufacturer) { $txtManufacturer.Text  = $compSys.Manufacturer }
+    if ($txtModel)        { $txtModel.Text         = $compSys.Model }
+    if ($txtSerial)       { $txtSerial.Text        = $bios.SerialNumber }
+    if ($txtBios)         { $txtBios.Text          = $bios.SMBIOSBIOSVersion }
+    if ($txtManModel)     { $txtManModel.Text      = "$($compSys.Manufacturer) - $($compSys.Model)" }
 
-    # Disable Dell Command Update if not Dell
-    if ($txtManufacturer.Text -ne "Dell Inc.") {
-        $chkDellCmd.IsEnabled = $false
-        $chkDellCmd.ToolTip = "This option is only available on Dell systems."
-        $chkDellCmd.Foreground = [System.Windows.Media.Brushes]::DarkSlateGray
-    } else {
-        $chkDellCmd.IsEnabled = $true
-        $chkDellCmd.ToolTip = $null
-        $chkDellCmd.Foreground = [System.Windows.Media.Brushes]::White
+    # Disable Dell Command Update if not Dell and make text visibly disabled
+    if ($txtManufacturer -and $chkDellCmd) {
+        if ($txtManufacturer.Text -ne "Dell Inc.") {
+            $chkDellCmd.IsEnabled = $false
+            $chkDellCmd.ToolTip = "This option is only available on Dell systems."
+            $chkDellCmd.Foreground = [System.Windows.Media.Brushes]::DarkSlateGray
+        } else {
+            $chkDellCmd.IsEnabled = $true
+            $chkDellCmd.ToolTip = $null
+            $chkDellCmd.Foreground = [System.Windows.Media.Brushes]::White
+        }
     }
 
     # Only show CPU as 'x64' or 'ARM'
     if ($proc.Architecture -eq 9) {
-        $txtCpu.Text = "x64"
-        $lblCpuLabel.Visibility = "Visible"
-        $txtCpu.Visibility = "Visible"
+        if ($txtCpu) { $txtCpu.Text = "x64" }
+        if ($lblCpuLabel) { $lblCpuLabel.Visibility = "Visible" }
+        if ($txtCpu) { $txtCpu.Visibility = "Visible" }
     } elseif ($proc.Architecture -eq 12) {
-        $txtCpu.Text = "ARM"
-        $lblCpuLabel.Visibility = "Visible"
-        $txtCpu.Visibility = "Visible"
+        if ($txtCpu) { $txtCpu.Text = "ARM" }
+        if ($lblCpuLabel) { $lblCpuLabel.Visibility = "Visible" }
+        if ($txtCpu) { $txtCpu.Visibility = "Visible" }
     } else {
-        $lblCpuLabel.Visibility = "Collapsed"
-        $txtCpu.Visibility = "Collapsed"
+        if ($lblCpuLabel) { $lblCpuLabel.Visibility = "Collapsed" }
+        if ($txtCpu) { $txtCpu.Visibility = "Collapsed" }
     }
 } catch {
-    $txtManufacturer.Text = "N/A"
-    $txtModel.Text        = "N/A"
-    $txtSerial.Text       = "N/A"
-    $txtBios.Text         = "N/A"
-    $lblCpuLabel.Visibility = "Collapsed"
-    $txtCpu.Visibility = "Collapsed"
-    $chkDellCmd.IsEnabled = $false
-    $chkDellCmd.ToolTip = "This option is only available on Dell systems."
+    if ($txtManufacturer) { $txtManufacturer.Text = "N/A" }
+    if ($txtModel)        { $txtModel.Text        = "N/A" }
+    if ($txtSerial)       { $txtSerial.Text       = "N/A" }
+    if ($txtBios)         { $txtBios.Text         = "N/A" }
+    if ($txtManModel)     { $txtManModel.Text     = "N/A" }
+    if ($lblCpuLabel)     { $lblCpuLabel.Visibility = "Collapsed" }
+    if ($txtCpu)          { $txtCpu.Visibility = "Collapsed" }
+    if ($chkDellCmd) {
+        $chkDellCmd.IsEnabled = $false
+        $chkDellCmd.ToolTip = "This option is only available on Dell systems."
+        $chkDellCmd.Foreground = [System.Windows.Media.Brushes]::DarkSlateGray
+    }
 }
 
 # Winget version check
 try {
     $wingetVersion = & winget --version 2>$null
     if ($wingetVersion) {
-        $txtWinget.Text = "Winget installed ($wingetVersion)"
-        $txtWinget.Foreground = [System.Windows.Media.Brushes]::LimeGreen
+        if ($txtWinget) {
+            $txtWinget.Text = "Winget installed ($wingetVersion)"
+            $txtWinget.Foreground = [System.Windows.Media.Brushes]::LimeGreen
+        }
     } else {
+        if ($txtWinget) {
+            $txtWinget.Text = "Winget not installed"
+            $txtWinget.Foreground = [System.Windows.Media.Brushes]::Red
+        }
+    }
+} catch {
+    if ($txtWinget) {
         $txtWinget.Text = "Winget not installed"
         $txtWinget.Foreground = [System.Windows.Media.Brushes]::Red
     }
-} catch {
-    $txtWinget.Text = "Winget not installed"
-    $txtWinget.Foreground = [System.Windows.Media.Brushes]::Red
 }
 
 # Set Time Zone display
 $timeZone = [System.TimeZoneInfo]::Local
-$lblTimeZone.Content = $timeZone.StandardName
+if ($lblTimeZone) { $lblTimeZone.Content = $timeZone.StandardName }
 
 # Timer to update the date and time every minute, showing: Friday, June 6 10:52 AM
 $timer = New-Object System.Windows.Threading.DispatcherTimer
 $timer.Interval = [TimeSpan]::FromMinutes(1)
 $updateDateTime = {
     $now = Get-Date
-    $lblTime.Content = $now.ToString("dddd, MMMM d  h:mm tt")
+    if ($lblTime) { $lblTime.Content = $now.ToString("dddd, MMMM d  h:mm tt") }
 }
 $timer.Add_Tick($updateDateTime)
 & $updateDateTime # Set immediately on load
 $timer.Start()
 
 # Make both labels look clickable
-$lblTime.ToolTip = "Click to open Windows Date & Time settings"
-$lblTimeZone.ToolTip = "Click to open Windows Date & Time settings"
+if ($lblTime) { $lblTime.ToolTip = "Click to open Windows Date & Time settings" }
+if ($lblTimeZone) { $lblTimeZone.ToolTip = "Click to open Windows Date & Time settings" }
 $openSettings = {
     Start-Process "ms-settings:dateandtime"
 }
-$null = $lblTime.Add_MouseLeftButtonUp($openSettings)
-$null = $lblTimeZone.Add_MouseLeftButtonUp($openSettings)
+if ($lblTime) { $null = $lblTime.Add_MouseLeftButtonUp($openSettings) }
+if ($lblTimeZone) { $null = $lblTimeZone.Add_MouseLeftButtonUp($openSettings) }
 
 # TPM Check (supports italics for 'Autopilot not supported')
 $tpmVer = Get-TpmVersion
-$lblTPM.Inlines.Clear()
-if ($tpmVer -and $tpmVer -match "^2") {
-    $lblTPM.Inlines.Add("TPM v2")
-    $lblTPM.Foreground = [System.Windows.Media.Brushes]::LimeGreen
-} else {
-    $lblTPM.Inlines.Add("TPM v2 ")
-    $italic = New-Object System.Windows.Documents.Run(" (Autopilot not supported)")
-    $italic.FontStyle = [System.Windows.FontStyles]::Italic
-    $lblTPM.Inlines.Add($italic)
-    $lblTPM.Foreground = [System.Windows.Media.Brushes]::Red
+if ($lblTPM) {
+    $lblTPM.Inlines.Clear()
+    if ($tpmVer -and $tpmVer -match "^2") {
+        $lblTPM.Inlines.Add("TPM v2")
+        $lblTPM.Foreground = [System.Windows.Media.Brushes]::LimeGreen
+    } else {
+        $lblTPM.Inlines.Add("TPM v2 ")
+        $italic = New-Object System.Windows.Documents.Run(" (Autopilot not supported)")
+        $italic.FontStyle = [System.Windows.FontStyles]::Italic
+        $lblTPM.Inlines.Add($italic)
+        $lblTPM.Foreground = [System.Windows.Media.Brushes]::Red
+    }
 }
 
 # Button Events
-$btnCancel.Add_Click({ $window.Close() })
-$btnContinue.Add_Click({
-    # Example: Show values of checkboxes, including Clear TPM
-    $summary = @"
+if ($btnCancel) {
+    $btnCancel.Add_Click({ $window.Close() })
+}
+if ($btnContinue) {
+    $btnContinue.Add_Click({
+        # Example: Show values of checkboxes, including Clear TPM
+        $summary = @"
 Selections:
 - Install Office Applications: $($chkOffice.IsChecked)
 - Install Cisco Umbrella: $($chkUmbrella.IsChecked)
 - Install Dell Command Update: $($chkDellCmd.IsChecked)
 - Clear TPM: $($chkClearTPM.IsChecked)
 "@
-    [System.Windows.MessageBox]::Show($summary, "Continue clicked! (Add your logic here)")
-})
+        [System.Windows.MessageBox]::Show($summary, "Continue clicked! (Add your logic here)")
+    })
+}
 
 $window.ShowDialog() | Out-Null
 $timer.Stop()
