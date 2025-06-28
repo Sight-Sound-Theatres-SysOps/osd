a838d59b5e79c9d3f8e988a2304ab02810ff885a
Matthew Miles <matthew.miles@sight-sound.com>
2025-06-28 11:59:43 -0500

    Update script version to 25.6.28.2 and modify Dell CommandUpdate installation command in OOBE functions

--- functions/oobe_menu_functions.ps1
+++ functions/oobe_menu_functions.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param()
 $ScriptName = 'oobe_menu_functions.ps1'
-$ScriptVersion = '25.6.28.1'
+$ScriptVersion = '25.6.28.2'
 
 #region Initialize
 if ($env:SystemDrive -eq 'X:') {
@@ -73,7 +73,7 @@ function step-oobeMenu_InstallDellCmd {
     param ()  
     
     Write-Host -ForegroundColor Yellow "[-] Installing Dell Commandupdate"
-    winget install --id Dell.CommandUpdate --accept-package-agreements --accept-source-agreements --silent
+    winget install --id Dell.CommandUpdate --accept-package-agreements --accept-source-agreements
 }
 
 
