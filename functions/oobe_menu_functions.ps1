e519aa9bf8aebebf6b575461cdb05277c9aa9ae6
Matthew Miles <matthew.miles@sight-sound.com>
2025-06-28 15:27:06 -0500

    Update script version to 25.6.28.4 and ensure proper execution of Dell CommandUpdate installation in OOBE functions

--- functions/oobe_menu_functions.ps1
+++ functions/oobe_menu_functions.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param()
 $ScriptName = 'oobe_menu_functions.ps1'
-$ScriptVersion = '25.6.28.3'
+$ScriptVersion = '25.6.28.4'
 
 #region Initialize
 if ($env:SystemDrive -eq 'X:') {
@@ -73,17 +73,10 @@ function step-oobeMenu_InstallDellCmd {
     param ()  
     
     Write-Host -ForegroundColor Yellow "[-] Installing Dell Commandupdate"
-    winget install --id Dell.CommandUpdate --accept-package-agreements --accept-source-agreements
+    powershell winget install --id Dell.CommandUpdate --accept-package-agreements --accept-source-agreements
     Write-Host -ForegroundColor Yellow "[-] Running Dell CommandUpdate to apply updates"
-    invoke-expression (Invoke-RestMethod -Uri "dell.sight-sound.dev")
-    invoke-dcu -applyUpdates -reboot Disable
-}
-
-function step-oobeMenu_ClearTPM {
-    [CmdletBinding()]
-    param ()   
-
-    Write-Host -ForegroundColor Yellow "[-] Clearing TPM"
+    powershell invoke-expression (Invoke-RestMethod -Uri "dell.sight-sound.dev")
+    powershell invoke-dcu -applyUpdates -reboot Disable
 }
 
 function step-oobeMenu_RegisterAutopilot {
