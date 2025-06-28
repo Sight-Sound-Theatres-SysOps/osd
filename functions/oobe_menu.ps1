b2468db00e371b724b79a4c516fe514b64046952
Matthew Miles <matthew.miles@sight-sound.com>
2025-06-28 09:41:22 -0500

    Update script version to 25.6.28.7 and adjust width of Computer Details box in OOBE menu

--- functions/oobe_menu.ps1
+++ functions/oobe_menu.ps1
@@ -55,7 +55,7 @@ function step-oobemenu {
             </Grid.RowDefinitions>
             <!-- Computer Details Box -->
             <Border Grid.Row="0" Margin="0,0,10,20" Padding="16" CornerRadius="10"
-                    Background="#FF1E1E1E" HorizontalAlignment="Center" Width="330">
+                    Background="#FF1E1E1E" HorizontalAlignment="Center" Width="380">
                 <StackPanel>
                     <TextBlock Text="Computer Details" FontSize="15" FontWeight="Bold"
                                Foreground="White" HorizontalAlignment="Center" Margin="0,0,0,2"/>
