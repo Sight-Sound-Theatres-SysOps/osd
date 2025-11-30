e17304fe710f2b376e52c91e96ac1ef364c001ea
Matthew Miles <matthew.miles@sight-sound.com>
2025-11-30 17:56:34 -0600

    Add Test-WebConnection function to verify network connectivity

--- functions/setupcomplete_functions.ps1
+++ functions/setupcomplete_functions.ps1
@@ -1,4 +1,31 @@
+function Test-WebConnection {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline)]
+        # Uri to test
+        [System.Uri]$Uri = 'google.com'
+    )
 
+    $Params = @{
+        Method = 'Head'
+        Uri = $Uri
+        UseBasicParsing = $true
+        Headers = @{'Cache-Control'='no-cache'}
+    }
+
+    try {
+        Write-Verbose "Test-WebConnection OK: $Uri"
+        Invoke-WebRequest @Params | Out-Null
+        $true
+    }
+    catch {
+        Write-Verbose "Test-WebConnection FAIL: $Uri"
+        $false
+    }
+    finally {
+        $Error.Clear()
+    }
+}
 Function Set-PowerSettingTurnMonitorOffAfter
 {
 	[CmdletBinding()]
